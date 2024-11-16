1a0a122530113a4483520aa1fe514d71d3f9082c
David Galiata | neoslash.net <david.galiata@aquia.io>
2024-11-15 17:22:46 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,24 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import { SITE_METADATA } from "./src/consts.ts";
 import metaTags from "astro-meta-tags";
-
 import robotsTxt from "astro-robots-txt";
+import { astroExpressiveCode } from '@expressive-code/astro';
 
-// https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: SITE_METADATA.siteUrl,
-  integrations: [mdx(), sitemap(), tailwind(), solidJs(), metaTags(), robotsTxt()]
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    solidJs(),
+    metaTags(),
+    robotsTxt(),
+    astroExpressiveCode({
+      theme: 'github-dark', // Change to the theme you want
+      features: {
+        copyButton: true, // Enable the "Copy Code" button
+      },
+    }),
+  ],
 });
