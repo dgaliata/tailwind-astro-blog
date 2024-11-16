4ce7b83451402cdf64f11200a2263794c221c9c9
David Galiata | neoslash.net <david.galiata@aquia.io>
2024-11-15 17:47:54 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,21 +8,29 @@ import metaTags from "astro-meta-tags";
 import robotsTxt from "astro-robots-txt";
 import expressiveCode from 'astro-expressive-code'
 
-// https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: SITE_METADATA.siteUrl,
-  integrations: [mdx(), sitemap(), tailwind(), solidJs(), metaTags(), robotsTxt(), expressiveCode({
-    themes: ['github-dark', 'github-light'],
-    styleOverrides: {
-      frames: {
-        editorActiveTabIndicatorTopColor: 'transparent',
-        editorActiveTabBorderColor: '#80808080',
-        editorTabBarBorderBottomColor: '#80808080',
-        tooltipSuccessBackground: 'black'
-      },
-      uiFontFamily: 'inherit',
-      borderColor: '#80808080'
-    }
-  }),]
+  integrations: [
+    // Move expressiveCode before mdx
+    expressiveCode({
+      themes: ['github-dark', 'github-light'],
+      styleOverrides: {
+        frames: {
+          editorActiveTabIndicatorTopColor: 'transparent',
+          editorActiveTabBorderColor: '#80808080',
+          editorTabBarBorderBottomColor: '#80808080',
+          tooltipSuccessBackground: 'black'
+        },
+        uiFontFamily: 'inherit',
+        borderColor: '#80808080'
+      }
+    }),
+    mdx(), 
+    sitemap(), 
+    tailwind(), 
+    solidJs(), 
+    metaTags(), 
+    robotsTxt()
+  ]
 });
